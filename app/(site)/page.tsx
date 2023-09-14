f7e3bdcf16e6241b5efa1010fb0eea73dddafc60
Zaid Bhati <59171704+Zaidbhati10114@users.noreply.github.com>
2023-09-14 12:03:31 +0530

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mb-2">
-          <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
+          <h1 className="text-white text-3xl font-semibold">Welcome Back <p className="text-sm text-green-500 py-3">Made By Zaid @ 2023</p></h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
             {/* ListItem */}
             <ListItem
